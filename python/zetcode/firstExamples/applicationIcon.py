e78fb62f3b9bf02d578631b9b2e0d8e67eaa38e3
adomfritz <adam.fries92@gmail.com>
2018-09-21 00:04:03 +0000

    Changing directory name

---
+++ python/zetcode/firstExamples/applicationIcon.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+
+ZetCode PyQt5 Tutorial
+
+This example shows an icon
+in the titlebar of the window.
+
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300,300,300,220)
+        self.setWindowTitle('Icon Example')
+        self.setWindowIcon(QIcon('web.png'))
+
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
