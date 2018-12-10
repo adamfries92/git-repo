dad0a08916c356752a8905e0356a96b58e6e3eb8
Adam <adam.fries92@gmail.com>
2018-12-10 03:50:42 +0000

    Basic example with templates and type deduction

--- c++/reactiveProgramming/chapter2/TypesAndTemplatesExample.cpp
+++ c++/reactiveProgramming/chapter2/TypesAndTemplatesExample.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <typeinfo>
+
+using namespace std;
+
+template<typename T>
+void EmitConsole(T value)
+{
+  cout << typeid(value).name() << ": " << value << endl;
+}
+
+template<typename T>
+void EmitValues(T&& arg)
+{
+  EmitConsole(forward<T>(arg));
+}
+
+template<typename T1, typename... Tn>
+void EmitValues(T1&& arg1, Tn&&... args)
+{
+  EmitConsole(forward<T1>(arg1));
+  EmitValues(forward<Tn>(args)...);
+}
+
+int main()
+{
+  EmitValues(0, 2.5, "Hello World!", 4);
+  return 0;
+}
